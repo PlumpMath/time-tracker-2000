9593e631441d4023cf6b069a7147eddb9c8d26bc
Thomas Crowley <crowley@kibu.com.au>
2014-03-05 23:24:30 +1100

    Primitive hiccup-style latex rendering

--- src/invoice/pdf.clj
+++ src/invoice/pdf.clj
@@ -1,2 +1,18 @@
 (ns invoice.pdf
   (:use [clojure.java.shell :only [sh]]))
+
+(defn writelatex [& txt]
+  (apply str (map to-latex txt)))
+
+(defn to-latex [v]
+  (cond
+   (keyword? v) (str "\\" (name v))
+   (vector? v) (str "{" (apply str (flatten (map to-latex v))) "}")
+   (string? v) (str v)))
+
+(defn % [s] (str "% " s "\n"))
+
+(writelatex :documentclass ["invoice"]
+            :def :tab [:hspace ["3ex"]]
+            :begin ["document"] (% "foo")
+            :hfil [:large :bf "Invoice 133"] :hifil)
